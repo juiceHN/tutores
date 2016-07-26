34840cfc74f2344c1479841ac433e980fae12a0c
JuanCarlosTapia <jc_tapia95@hotmail.com>
2016-07-26 12:52:44 -0600

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,36 @@
+public class Usuario {
+	//atributos
+	private String nombre, contrasena;
+	//constructor
+	private int id;
+	public Usuario(String nombre, String contrasena, int id) {
+		super();
+		this.nombre = nombre;
+		this.contrasena = contrasena;
+		this.id = id;
+	
+	}
+	//sets y gets
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
+	
+	
+}

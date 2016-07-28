34bbaa993e416a4a6e97585d929af98d7c265330
RRodCastro <mrcastro9574@gmail.com>
2016-07-27 23:49:59 -0600

    Create SecondForm.java

---
+++ SecondForm.java
@@ -0,0 +1,20 @@
+package apps.roddcastro.com.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Rodrigo on 27/07/2016.
+ */
+public class SecondForm extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondform);
+
+    }
+}

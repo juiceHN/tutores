5d26dcaf9cf4566dbdf4300d33d6541833bfe224
RRodCastro <mrcastro9574@gmail.com>
2016-08-17 23:22:25 -0600

    Create user.service.js

---
+++ LogIn/services/user.service.js
@@ -0,0 +1,143 @@
+var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('users');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getById = getById;
+service.create = create;
+service.update = update;
+
+
+module.exports = service;
+
+function authenticate(username, password) {
+    var deferred = Q.defer();
+
+    db.users.findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            //Ususario y contrasena encontradas
+            deferred.resolve(jwt.sign({ sub: user._id }, config.secret));
+        } else {
+            // No se encontro en la base de datos
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user) {
+            
+            deferred.resolve(_.omit(user, 'hash'));
+        } else {
+            
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function create(userParam) {
+    var deferred = Q.defer();
+
+    // validacion
+    db.users.findOne(
+        { username: userParam.username },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user) {
+                //Error de usuario duplicado
+                deferred.reject('Nombre de usuario "' + userParam.username + '" ya ha sido utilizado');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        
+        var user = _.omit(userParam, 'password');
+
+       
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+
+        db.users.insert(
+            user,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+
+    // Validacion de datos (Opcion de actualizar)
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user.username !== userParam.username) {
+            
+            db.users.findOne(
+                { username: userParam.username },
+                function (err, user) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (user) {
+                        
+                        deferred.reject('Nombre de usuario "' + req.body.username + '" ya existe')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+       
+        var set = {
+            firstName: userParam.firstName,
+            lastName: userParam.lastName,
+            username: userParam.username,
+        };
+
+        
+        if (userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password, 10);
+        }
+
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+

b71dec0c769ab66df14124a6ddfe7cd15c8df17e
RRodCastro <mrcastro9574@gmail.com>
2016-11-15 17:02:54 -0600

    Create flash.services.js

---
+++ Administrador/app/app-services/flash.services.js
@@ -0,0 +1,53 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('FlashService', Service);
+
+    function Service($rootScope) {
+        var service = {};
+
+        service.Success = Success;
+        service.Error = Error;
+
+        initService();
+
+        return service;
+
+        function initService() {
+            $rootScope.$on('$locationChangeStart', function () {
+                clearFlashMessage();
+            });
+
+            function clearFlashMessage() {
+                var flash = $rootScope.flash;
+                if (flash) {
+                    if (!flash.keepAfterLocationChange) {
+                        delete $rootScope.flash;
+                    } else {
+                        // Cambiar la localidad, en caso de falso
+                        flash.keepAfterLocationChange = false;
+                    }
+                }
+            }
+        }
+
+        function Success(message, keepAfterLocationChange) {
+            $rootScope.flash = {
+                message: message,
+                type: 'success', 
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+        }
+
+        function Error(message, keepAfterLocationChange) {
+            $rootScope.flash = {
+                message: message,
+                type: 'danger',
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+        }
+    }
+
+})();
